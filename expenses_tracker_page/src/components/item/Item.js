4990cd36ca264d5a2ad09a6e1bd416999556ce4c
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-12-04 17:40:15 +0200

    Add CardIdem

---
+++ expenses_tracker_page/src/components/item/Item.js
@@ -0,0 +1,26 @@
+import MoreIcon from "./MoreIcon";
+import "./item.scss";
+
+const ApiItem = ({ item }) => {
+  return (
+    <div className="api-item" style={{ borderTopColor: item.color }}>
+      <div className="item-header">
+        <h4>{item.title}</h4>
+        <MoreIcon />
+      </div>
+
+      <div className="item-info">
+        <div className="blance">
+          <span className="text">Balance Due</span>
+          <span className="number">{item.info.blance}</span>
+        </div>
+        <div className="blance">
+          <span className="text">total invoiced</span>
+          <span className="number">{item.info.total}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApiItem;
---
+++ expenses_tracker_page/src/data/index.js
@@ -0,0 +1,76 @@
+const data = [
+  {
+    id: 1,
+    title: "Reactial",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: "#5FBCE3",
+  },
+  {
+    id: 2,
+    title: "Promotional",
+    info: {
+      blance: "$0",
+      total: "$3200",
+    },
+    color: "#9A7FFA",
+  },
+  {
+    id: 3,
+    title: "Traveller",
+    info: {
+      blance: "$0",
+      total: "$19,000",
+    },
+    color: "#FF9458",
+  },
+  {
+    id: 4,
+    title: "Normal",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: "#E35F67",
+  },
+  {
+    id: 5,
+    title: "Reactial",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: "#9EC84D",
+  },
+  {
+    id: 6,
+    title: "Promotional",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: "#FA867F",
+  },
+  {
+    id: 7,
+    title: "Traveller",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: "#CC58FF",
+  },
+  {
+    id: 8,
+    title: "Normal",
+    info: {
+      blance: "$230",
+      total: "$12,650",
+    },
+    color: " #7693FF",
+  },
+];
+
+export default data;
