1ff11a8fed1942b26aa8d5c0cfa2912d7eb6f6d7
omur8819 <omur8819@gmail.com>
2020-12-07 17:15:43 +0300

    PrimarySearchAppBar and Customer components added

--- expenses_tracker_page/src/App.js
+++ expenses_tracker_page/src/App.js
@@ -2,11 +2,15 @@ import { SearchBar } from "./components/searchbar/SearchBar";
 
 import Item from "./components/item/Item";
 import data from "./data/index";
+import PrimarySearchAppBar from './components/PrimarySearchAppBar/PrimarySearchAppBar';
+import Customer from './components/Customer/Customer';
 import "./App.css";
 
 function App() {
   return (
     <>
+      <PrimarySearchAppBar />
+      <Customer />
       <SearchBar />
       <div className="card-list">
         {data?.map((item) => (
